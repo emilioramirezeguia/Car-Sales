b22eb2e7e6e93b99b0da71e422d83d1eb1abc525
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-07-14 12:10:30 -0700

    Connect components to store and map corresponding state properties

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import AdditionalFeature from './AdditionalFeature';
+import React from "react";
+import { connect } from "react-redux";
+import AdditionalFeature from "./AdditionalFeature";
 
-const AdditionalFeatures = props => {
+// This component needs the additionalFeatures array from state
+const AdditionalFeatures = (props) => {
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {props.additionalFeatures.map((item) => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
@@ -18,4 +20,10 @@ const AdditionalFeatures = props => {
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = (state) => {
+  return {
+    additionalFeatures: state.additionalFeatures,
+  };
+};
+
+export default connect(mapStateToProps, {})(AdditionalFeatures);
